9ab6cfe1578e81479c4e743414176561238585d0
Максим Майнагашев <maxuser99@mail.ru>
2021-07-14 23:34:21 +0700

    Player and Enemy added

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private int _health;
+
+    public void ApplyDamage(int damage)
+    {
+        _health -= damage;
+        if (_health <= 0)
+            Die();
+    }
+
+    public void Die()
+    {
+    
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    
+    private void Update()
+    {
+        
+    }
+}
